aa2006daec648e1a4c8e0bae1cfc225b8b2cf8d8
Hello SRL <hellosrl@Hellos-MacBook-Pro.local>
2025-02-24 11:05:38 -0800

    mod:Index name

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
-import { Routes, Route, Link } from 'react-router-dom';
-import { HomePage } from './pages/HomePage';
-import { ProductsPage } from './pages/ProductsPage';
-import Isologo from './assets/Isologo Fondo Negro SVG.svg'
+import { Routes, Route, Link } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+import { ProductsPage } from "./pages/ProductsPage";
+import Navbar from "./components/Navbar";
+import { AuthProvider } from "./context/AuthContext";
+import { CartProvider } from "./context/CartContext";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-100">
-      {/* Header */}
-      <header className="bg-primary shadow sticky top-0 z-20">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-6 py-4">
-          <div className="flex items-center justify-between">
-            <Link to="/" className="flex items-center">
-            <img src={Isologo} alt="Logo WeTECH" className="h-24 w-auto" />
-            </Link>
-          </div>
-        </div>
-      </header>
+      <AuthProvider>
+        <CartProvider>
+          <Navbar />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsPage />} />
-      </Routes>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products" element={<ProductsPage />} />
+          </Routes>
+        </CartProvider>
+      </AuthProvider>
     </div>
   );
 }
 
-export default App
+export default App;
