0892299c7b3bc5efb433bf0dce5aa29e6dcdd1af
Hello SRL <hellosrl@Hellos-MacBook-Pro.local>
2025-03-10 16:17:00 -0300

    imp:navbar responsitivty add: location combobox

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,18 @@
 import { useState } from "react";
 import { ShoppingCart, UserCircle } from "lucide-react";
+import { FaMapMarkerAlt, FaWhatsapp } from "react-icons/fa";
 import { useCart } from "../context/CartContext";
 import { useAuth } from "../context/AuthContext";
 import CartModal from "../components/CartModal";
 import LoginModal from "../components/LoginModal";
 import { Link, useNavigate } from "react-router-dom";
 import Isologo from "../assets/Isologo Fondo Negro SVG.svg";
-import { FaWhatsapp } from "react-icons/fa";
 
 export default function Navbar() {
   const [isCartOpen, setIsCartOpen] = useState(false);
   const [isLoginOpen, setIsLoginOpen] = useState(false);
   const [searchQuery, setSearchQuery] = useState("");
+  const [selectedProvince, setSelectedProvince] = useState("Mendoza");
   const { items } = useCart();
   const { isAuthenticated, logout } = useAuth();
   const navigate = useNavigate();
@@ -32,21 +33,41 @@ export default function Navbar() {
     navigate(`/search?query=${encodeURIComponent(searchQuery)}`);
   };
 
+  const handleProvinceChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setSelectedProvince(e.target.value);
+  };
+
   return (
     <>
       {/* Header */}
       <header className="bg-primary shadow sticky top-0 z-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
           <div className="flex items-center justify-between">
-            <Link to="/" className="flex items-center">
-            <img src={Isologo} alt="Logo WeTECH" className="h-20 w-auto" />
+            <Link to="/" className="flex items-center pr-2">
+              <img src={Isologo} alt="Logo WeTECH" className="h-20 w-auto" />
             </Link>
-            {/* Barra de busqueda */}
-            <form onSubmit={handleSearch} className="flex-grow mx-2 max-w-md">
+            <div className="relative flex items-center space-x-2">
+              <FaMapMarkerAlt className="absolute left-3 text-white" />
+              <select
+                value={selectedProvince}
+                onChange={handleProvinceChange}
+                className="p-2 pl-6 border rounded-md bg-black text-white w-32 max-w-xs"
+              >
+                <option value="Mendoza">Mendoza</option>
+                <option value="Buenos Aires">Buenos Aires</option>
+                <option value="Córdoba">Córdoba</option>
+                <option value="Santa Fe">Santa Fe</option>
+              </select>
+            </div>
+            {/* Barra de busqueda para computadoras */}
+            <form
+              onSubmit={handleSearch}
+              className="hidden lg:flex flex-grow mx-2 max-w-lg"
+            >
               <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
                 Search
               </label>
-              <div className="relative">
+              <div className="relative flex-grow">
                 <input
                   type="search"
                   value={searchQuery}
@@ -56,7 +77,7 @@ export default function Navbar() {
                 />
                 <button
                   type="submit"
-                  className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                  className="text-white absolute right-2.5 bottom-2.5 bg-black hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
                   <svg
                     className="w-4 h-4 text-white"
@@ -78,6 +99,7 @@ export default function Navbar() {
             </form>
 
             <div className="flex items-center space-x-2 pl-2 sm:space-x-4">
+              
               {isAuthenticated ? (
                 <button
                   onClick={logout}
@@ -108,6 +130,46 @@ export default function Navbar() {
               </button>
             </div>
           </div>
+        </div>
+        {/* Barra de busqueda para celulares */}
+        <div className="pb-2 px-4">
+          <form
+            onSubmit={handleSearch}
+            className="flex lg:hidden flex-grow mx-2 max-w-lg"
+          >
+            <label className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white">
+              Search
+            </label>
+            <div className="relative flex-grow">
+              <input
+                type="search"
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)}
+                placeholder="Buscar "
+                className="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+              <button
+                type="submit"
+                className="text-white absolute right-2.5 bottom-2.5 bg-black hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                <svg
+                  className="w-4 h-4 text-white"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 20 20"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                  />
+                </svg>
+              </button>
+            </div>
+          </form>
         </div>
       </header>
 
