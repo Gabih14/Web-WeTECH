3b735d1eadd5b0e1ff2c221c75bc7989fe22f06a
Hello SRL <hellosrl@Hellos-MacBook-Pro.local>
2025-03-18 16:13:17 -0300

    add: v7_startTransition

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter
+      future={{
+        v7_startTransition: true,
+      }}
+    >
       <App />
     </BrowserRouter>
   </StrictMode>
