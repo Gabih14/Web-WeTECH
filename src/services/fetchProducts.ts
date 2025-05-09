2b9a7bd06fa43dba6c4d3f0b11d4c878285e01a7
Hello SRL <hellosrl@Hellos-MacBook-Pro.local>
2025-05-09 16:34:43 -0300

    fix subcategories

--- src/services/fetchProducts.ts
+++ src/services/fetchProducts.ts
@@ -9,7 +9,7 @@ export const fetchProducts = (): Promise<Product[]> => {
 
       rawProducts.forEach((item) => {
         const familiaId = item.familiaId || item.id; // Si no tiene familiaId, usar el id como único producto
-console.log(item);
+        //console.log(item);
         if (!groupedProducts[familiaId]) {
           // Crear el producto principal
           groupedProducts[familiaId] = {
@@ -18,7 +18,7 @@ console.log(item);
             description: item.descripcion,
             image: "", // Puedes asignar una imagen genérica o específica si está disponible
             category: item.grupo,
-            subcategory: item.subgrupo || undefined,
+            subcategory: item.subgrupo ? item.subgrupo.toUpperCase() : undefined,
             price: parseFloat(item.precioVtaCotizado || "0"), // Guardar precioVtaCotizado en todos los productos
             ...(item.grupo === "FILAMENTOS" && { colors: [] }), // Solo agregar `colors` si es "FILAMENTOS"
             ...(item.grupo !== "FILAMENTOS" && { stock: 0 }), // Solo agregar `stock` si no es "FILAMENTOS"
